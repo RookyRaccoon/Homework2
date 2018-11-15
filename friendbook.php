f914f5d4a745dce4249e201d4de556c52a6c7a8a
RookyRaccoon <37588542+RookyRaccoon@users.noreply.github.com>
2018-11-15 16:48:23 +0100

    Add files via upload

---
+++ friendbook.php
@@ -0,0 +1,52 @@
+<!DOCTYPE>
+<html>
+    
+     <head>
+     <meta charset="utf-8">
+
+          <title>Friend book</title>
+
+     </head>
+     <body>
+          <h1>Friend book </h1>
+          <form action="index.php" method="post">
+          Name: <input type="text" name="name">
+          <input type="submit" value="send">
+          <input type="text" name="nameFilter" value="<?php if(empty($_POST['nameFilter'])) $nameFilter = NULL;?>">
+          <input type="submit" value="filter">
+          </form>
+
+          <?php
+           $filename = 'friend.txt';
+
+          
+
+           $name =$_POST['name'];
+           $file = fopen( $filename, "a" );
+           if("$name"!=NULL){
+           	fwrite( $file, "$name\n" );
+           }
+           
+            
+           $file = fopen( $filename, "r" );
+           $nameFilter =$_POST['nameFilter'];
+           $file2 = fopen( $filename, "r" );
+           while (!feof($file)) {
+           	if ($nameFilter!=NULL){
+                 if(strstr(fgets($file),"$nameFilter",false)!=NULL){
+                 	$ligne=fgets($file2)."<br/>";
+                 	echo $ligne;
+
+                 }
+                 else{
+                 	fgets($file2);
+                 }
+           	}
+           	else{
+           		 echo fgets($file)."<br/>";
+           	}
+           }
+
+          ?>
+     </body>
+</html>
